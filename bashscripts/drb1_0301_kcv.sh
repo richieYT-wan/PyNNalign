67a324ec92b69f27919d8b7a0edbeb5fec79a599
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-05-24 23:12:42 +0200

    Update drb1_0301_kcv.sh

--- bashscripts/drb1_0301_kcv.sh
+++ bashscripts/drb1_0301_kcv.sh
@@ -12,7 +12,7 @@ do
   cd \${PYDIR}
   pwd
   echo \"Starting PyScript\"
-  python3 python3 train_model.py -f ../data/NetMHCIIpan_train/drb1_0301.csv -kf ${f} -o 230425_run_MHC -enc BL50LO -ml 21 -pad -15 -nh 500 -std True -bn True -do 0.0 -ws 9 -lr 1e-4 -bs 256 -ne 25 -tol 0.0001" > "230524_DRB1_kcv_f${f}.sh"
+  python3 ./train_model.py -f ../data/NetMHCIIpan_train/drb1_0301.csv -kf ${f} -o 230425_run_MHC -enc BL50LO -ml 21 -pad -15 -nh 500 -std True -bn True -do 0.0 -ws 9 -lr 1e-4 -bs 256 -ne 25 -tol 0.0001" > "230524_DRB1_kcv_f${f}.sh"
   chmod +x "230524_DRB1_kcv_f${f}.sh"
   qsub -W group_list=vaccine -A vaccine -m e -l nodes=1:ppn=1:thinnode,mem=10gb,walltime=00:10:00 "230524_DRB1_kcv_f${f}.sh"
 done
