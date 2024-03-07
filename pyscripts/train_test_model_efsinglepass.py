dbc834a424cc9b35f75826be20af3b5654c6e55c
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2024-03-07 16:25:18 +0100

    cleanup

--- pyscripts/train_test_model_efsinglepass.py
+++ pyscripts/train_test_model_efsinglepass.py
@@ -148,7 +148,7 @@ def main():
     connector = '' if args["out"] == '' else '_'
     kf = 'XX' if args["fold"] is None else args['fold']
     rid = args['random_id'] if (args['random_id'] is not None and args['random_id'] != '') \
-        else get_random_id() if args['random_id'] == '' else args['random_id']
+        else get_random_id() if (args['random_id'] == '' or args['random_id'] is None) else args['random_id']
     unique_filename = f'{args["out"]}{connector}KFold_{kf}_{get_datetime_string()}_{rid}'
 
     checkpoint_filename = f'checkpoint_best_{unique_filename}.pt'
