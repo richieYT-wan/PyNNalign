0ffb343ef698d1e6c62412dbec5fab85141260b2
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2025-01-15 15:16:14 +0100

    Update 250115_RedoBenchmarkNNalignCversion.sh

--- bashscripts/250115_RedoBenchmarkNNalignCversion.sh
+++ bashscripts/250115_RedoBenchmarkNNalignCversion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Base command without the parameters that will change
-base_command="python train_test_model_efsinglepass.py -trf ../data/mhc1_el_subsample/mhc1_el_1M_structure.csv -tef ../data/test_sets_ligands_abelin_wo_context/merged_test_sets_ligands_abelin.csv -ml 13 -ws 9 -pad -20 -y target -x sequence -std False -bn False -nh 54 -br 15 -otf True -cuda True -bs 1024 -ne 500 -wd 0 -add_ps True -indel True --add_pfr True --add_fr_len True --add_pep_len True --add_structure False -lr 1e-5"
+base_command="python train_test_model_efsinglepass.py -trf ../data/mhc1_el_subsample/mhc1_el_1M_structure.csv -tef ../data/test_sets_ligands_abelin_wo_context/merged_test_sets_ligands_abelin_no_U.csv -ml 13 -ws 9 -pad -20 -y target -x sequence -std False -bn False -nh 54 -br 15 -otf True -cuda True -bs 1024 -ne 500 -wd 0 -add_ps True -indel True --add_pfr True --add_fr_len True --add_pep_len True --add_structure False -lr 1e-5"
 
 # Generate a random ID for the job
 rid=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5)
@@ -27,7 +27,7 @@ done
 
 
 # Base command without the parameters that will change
-base_command="python train_test_model_efsinglepass.py -trf ../data/mhc1_el_subsample/mhc1_el_1M_structure.csv -tef ../data/test_sets_ligands_abelin_wo_context/merged_test_sets_ligands_abelin.csv -ml 13 -ws 9 -pad -20 -y target -x sequence -std False -bn False -nh 54 -br 10 -otf True -cuda True -bs 1024 -ne 500 -wd 0 -add_ps True -indel True --add_pfr True --add_fr_len True --add_pep_len True --add_structure False -lr 1e-5 -bn True -do 0.25 -add_hl True -nh2 27"
+base_command="python train_test_model_efsinglepass.py -trf ../data/mhc1_el_subsample/mhc1_el_1M_structure.csv -tef ../data/test_sets_ligands_abelin_wo_context/merged_test_sets_ligands_abelin_no_U.csv -ml 13 -ws 9 -pad -20 -y target -x sequence -std False -bn False -nh 54 -br 10 -otf True -cuda True -bs 1024 -ne 500 -wd 0 -add_ps True -indel True --add_pfr True --add_fr_len True --add_pep_len True --add_structure False -lr 1e-5 -bn True -do 0.25 -add_hl True -nh2 27"
 
 # Generate a random ID for the job
 rid=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5)
